eeb91c09acae37db74a5d62336766cd3a057df0e
davideCoderblock <davide@coderblock.com>
2023-11-09 09:01:50 +0100

    (feat work in progress)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,13 @@ import { UserButton } from "@clerk/nextjs";
 
 const SetupPage = () => {
   return (
-    <div className="p-4">
-      <UserButton afterSignOutUrl="/"/>
+    <div className="h-screen relative bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url('/media/work-in-progress-2.png')` }}>
+      <div className="absolute top-4 right-4">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+      <div className="flex items-center h-full mx-auto p-4">
+        {/* Contenuto aggiuntivo al centro */}
+      </div>
     </div>
   );
 };
