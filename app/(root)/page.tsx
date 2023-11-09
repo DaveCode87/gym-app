40d5906a1138d5a11e36f55d16ec5d22e90c2caf
Davide87 <davidetrovato87@gmail.com>
2023-11-09 12:22:03 +0100

    (minor fix)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,9 @@ import { UserButton } from "@clerk/nextjs";
 const SetupPage = () => {
   return (
     <div className="h-screen relative bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url('/media/work-in-progress-2.png')` }}>
-      <div className="absolute top-4 right-4">
+      
+      <div className="absolute top-4 right-4 flex flex-row">
+      <span className="mr-4">welcome</span>
         <UserButton afterSignOutUrl="/" />
       </div>
       <div className="flex items-center h-full mx-auto p-4">
